6e0428784b413ba80c19296b8baa7bdeb35fd822
John Kim A. Querobines <querobinesj@gmail.com>
2022-02-18 21:31:26 +0800

    contact form

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -10,7 +10,18 @@ const Contact = () => {
         <span className="section__subtitle">Send an email</span>
         <h3 className="section__title">Contact Form</h3>
 
-        <div className="contact container grid"></div>
+        <div className="contact container grid">
+          <input type="text" placeholder="Name" className="contact__input" />
+          <input type="mail" placeholder="Email" className="contact__input" />
+          <textarea
+            name=""
+            id=""
+            cols="0"
+            rows="10"
+            className="contact__input"
+          ></textarea>
+          <button className="button">Submit</button>
+        </div>
       </section>
     </>
   );
