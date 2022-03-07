94e4ea672b6c83e68ce025b80d21736aa3c3c5a6
John Kim A. Querobines <querobinesj@gmail.com>
2022-03-07 18:33:14 +0800

    google verification

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,6 +8,10 @@ const Layout = ({ children }) => {
     <>
       <Helmet>
         <body className={darkMode ? "dark-theme" : "light-theme"} />
+        <meta
+          name="google-site-verification"
+          content="-9qqJpKGshRJPseMtQESR_Sy1WYEQ_w2ZZSk6_f-Efg"
+        />
         <meta name="description" content="A web portfolio website of kerubi" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
       </Helmet>
