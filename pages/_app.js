6e5c3854fbab433cf41782f65e363ff4609bba39
John Kim A. Querobines <querobinesj@gmail.com>
2022-03-10 16:30:47 +0800

    moves head tag to _app js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import Layout from "../components/Layout";
+import Head from "next/head";
 import "../styles/index.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="-9qqJpKGshRJPseMtQESR_Sy1WYEQ_w2ZZSk6_f-Efg"
+        />
+        <meta
+          name="description"
+          content="A web portfolio website of John Kim A. Querobines"
+        />
+        <meta name="keywords" content="Portfolio website, Web Developer" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
