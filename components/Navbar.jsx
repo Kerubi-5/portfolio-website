dbb0ff2cf18023cc2147ac7ab34c27f4dd0e44b4
John Kim A. Querobines <querobinesj@gmail.com>
2022-03-19 17:22:53 +0800

    adds aria label to all links in navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -17,42 +17,53 @@ const Navbar = ({ darkMode, setDarkMode }) => {
     <header className="header" id="header">
       <nav className="nav container">
         <Link href="/">
-          <a className="nav__logo">KK</a>
+          <a className="nav__logo" aria-label={"Logo"}>
+            KK
+          </a>
         </Link>
 
         <div className="nav__menu">
           <ul className="nav__list">
             <li className="nav__item">
               <Link href="/">
-                <a className={activeLink("/")}>
+                <a className={activeLink("/")} aria-label={"Landing Page"}>
                   <UilEstate />
                 </a>
               </Link>
             </li>
             <li className="nav__item">
               <Link href="/about">
-                <a className={activeLink("/about")}>
+                <a className={activeLink("/about")} aria-label={"About me"}>
                   <UilUser />
                 </a>
               </Link>
             </li>
             <li className="nav__item">
               <Link href="/skills">
-                <a className={activeLink("/skills")}>
+                <a
+                  className={activeLink("/skills")}
+                  aria-label={"Skills and tech stacks"}
+                >
                   <UilCrosshair />
                 </a>
               </Link>
             </li>
             <li className="nav__item">
               <Link href="/works">
-                <a className={activeLink("/works")}>
+                <a
+                  className={activeLink("/works")}
+                  aria-label={"List of John Kim's works and projects"}
+                >
                   <UilBriefcaseAlt />
                 </a>
               </Link>
             </li>
             <li className="nav__item">
               <Link href="/contact">
-                <a className={activeLink("/contact")}>
+                <a
+                  className={activeLink("/contact")}
+                  aria-label={"Contact form to send email to the owner"}
+                >
                   <UilPhone />
                 </a>
               </Link>
